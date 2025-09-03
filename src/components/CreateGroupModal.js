9343e70646d4c120a4f67bfe81d33d980b5274cd
sharad <sharad.19552@knit.ac.in>
2025-09-03 21:11:49 +0530

    Fixed edit group button

--- src/components/CreateGroupModal.js
+++ src/components/CreateGroupModal.js
@@ -148,6 +148,7 @@ export default function CreateGroupModal({isModalOpen,closeModal,SetIsGroupCreat
                                 <button onClick={type == "create" ? createGroup : editGroup}
                                     className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                                 >
+                                    {type === "create" && 
                                     <svg
                                         className="me-1 -ms-1 w-5 h-5"
                                         fill="currentColor"
@@ -159,7 +160,7 @@ export default function CreateGroupModal({isModalOpen,closeModal,SetIsGroupCreat
                                             d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
                                             clipRule="evenodd"
                                         ></path>
-                                    </svg>
+                                    </svg>}
                                     {type === "create" ? "Create" : "Edit"}
                                 </button>
                             </div>
